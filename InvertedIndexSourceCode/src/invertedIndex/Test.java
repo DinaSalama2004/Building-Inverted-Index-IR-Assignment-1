7311d9e32aa0ba47a1b1dcdc3f02cc05bab25afa
nadaassad <136029252+nadaassad@users.noreply.github.com>
2025-03-18 01:56:41 +0200

    main part1

--- InvertedIndexSourceCode/src/invertedIndex/Test.java
+++ InvertedIndexSourceCode/src/invertedIndex/Test.java
@@ -19,7 +19,7 @@ public class Test {
         Index5 index = new Index5();
         //|**  change it to your collection directory 
         //|**  in windows "C:\\tmp11\\rl\\collection\\"       
-        String files = "C:\\test\\";
+        String files = "C:\\Users\\hp\\Desktop\\New folder (2) - Copy\\";
         File file = new File(files);
         //|** String[] 	list()
         //|**  Returns an array of strings naming the files and directories in the directory denoted by this abstract pathname.
