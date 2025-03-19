ee7661db8d277c2dbc3431680f39628a0df75e3d
NourAhmed <147117917+akarin26@users.noreply.github.com>
2025-03-19 17:04:15 +0200

    Update Test.java

--- InvertedIndexSourceCode/src/invertedIndex/Test.java
+++ InvertedIndexSourceCode/src/invertedIndex/Test.java
@@ -64,6 +64,37 @@ public class Test {
     }
 
 
+    public static void test_intersect(int[] arr1, int[] arr2) {
+        System.out.println("\nTest intersect : " );
+
+        // Convert arrays to linked lists
+
+        if (arr1.length == 0 ||arr2.length == 0)
+        {
+            System.out.println("Error: The array is empty.");
+            return;
+        }
+
+
+        Posting pL1= new Posting(arr1[0]);
+        Posting pL2= new Posting(arr2[0]);
+
+        Posting current1 = pL1;
+        for (int i = 1; i < arr1.length; i++) {
+            current1.next = new Posting(arr1[i]);
+            current1 = current1.next;
+        }
+        Posting current2 = pL2;
+        for (int i = 1; i < arr2.length; i++) {
+            current2.next = new Posting(arr2[i]);
+            current2 = current2.next;
+        }
+
+        // Perform intersection
+        return printPostingList(intersect(pL1, pL2)) ;
+    }
+    
+
     public static void main(String args[]) throws IOException {
         Index5 index = new Index5();
         //|**  change it to your collection directory
@@ -93,6 +124,19 @@ public class Test {
         index.buildIndex(fileList);
         index.store("index");
         index.printDictionary();
+
+        Posting p1 = new Posting(2);
+        Posting p2 = new Posting(5);
+        Posting p3 = new Posting(8);
+        Posting p4 = new Posting(11);
+
+        p1.next = p2;
+        p2.next = p3;
+        p3.next = p4;
+
+        System.out.print("Posting List Output: ");
+        index.printPostingList(p1);
+        
 
         String test3 = "data  should plain greatest comif"; // data  should plain greatest comif
         System.out.println("Boo0lean Model result = \n" + index.find_24_01(test3));
